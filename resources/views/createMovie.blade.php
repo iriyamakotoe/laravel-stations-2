88f0e9fcb7e6a497ff130c72c0e3ae8fa333cc9e
iriyamakotoe <iriyamakotoe@icloud.com>
2024-11-01 10:47:30 +0900

    PHP19_予約管理画面を作ろう

--- resources/views/createMovie.blade.php
+++ resources/views/createMovie.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>登録画面</title>
+    <title>映画登録画面</title>
 </head>
 <body>
 @if ($errors->any())
