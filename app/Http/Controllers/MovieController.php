36e3871b1ec8d7ca8ee6ef6daf0766464860a413
iriyamakotoe <iriyamakotoe@icloud.com>
2025-05-16 10:49:31 +0900

    22_SQL Injection の脅威を知ろう

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -33,14 +33,14 @@ class MovieController extends Controller
         }
 
         // ページネーションを設定（例：1ページあたり15件）
-        $movies = $query->paginate(20)->appends([
-            'keyword' => $keyword,
-            'is_showing' => $isShowing
-        ]);
+        // $movies = $query->paginate(20)->appends([
+        //     'keyword' => $keyword,
+        //     'is_showing' => $isShowing
+        // ]);
 
 // 作品データの変数名は、 view に渡している変数名と合わせて適宜変更してください
-// $movies = DB::table('movies')->whereRaw("title = {$request->input('keyword')}")->get();
-// $movies = DB::table('movies')->whereRaw("title = ?", [$request->input('keyword')])->get();
+// $movies = DB::table('movies')->whereRaw("title = '{$request->input('keyword')}'")->get();
+$movies = DB::table('movies')->whereRaw("title = ?", [$request->input('keyword')])->get();
 
         return view('index', [
             'movies' => $movies,
